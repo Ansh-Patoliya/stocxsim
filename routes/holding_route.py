c69168fcd4549e19abbb8b222ba5fddbca8f27a9
Dharmil <dharmil132@gmail.com>
2026-01-22 20:27:59 +0530

    Add holding route and integrate user holdings functionality

--- routes/holding_route.py
+++ routes/holding_route.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, jsonify, session
-from service.holding_service import get_holdings_by_user
+# from service.holding_service import get_holdings_by_user
+from database.holding_dao import get_holdings_by_user
 
 
 holding_bp = Blueprint('holding_bp', __name__)
+
 
 @holding_bp.route("/order", methods=["POST"])
 def get_user_holdings():
     try:
         user_id = session.get("user_id")
+        print("User ID in session:", user_id)
+
+        if not user_id:
+            return jsonify({"error": "User not logged in"}), 401
 
         holdings = get_holdings_by_user(user_id)
 
@@ -16,4 +22,6 @@ def get_user_holdings():
         })
 
     except Exception as e:
+        import traceback
+        traceback.print_exc()
         return jsonify({"error": str(e)}), 400
